93d1c94cf32da25e6c692138fc2309269a345bf2
SergeyKhval <sergeykhval@gmail.com>
2016-02-24 17:54:24 +0300

    added basic tod classes

--- es6/app/scripts/main.js
+++ es6/app/scripts/main.js
@@ -1,1 +1,40 @@
-console.log('\'Allo \'Allo!'); // eslint-disable-line no-console
+'use strict';
+
+class TodoApp {
+  constructor() {
+  }
+}
+
+class TodoList {
+  constructor() {
+    this.todoList = [];
+  }
+
+  addTodo(todo) {
+    this.todoList.push(todo);
+  }
+
+  removeTodoAt(index) {
+    this.todoList.splice(index, 1);
+  }
+
+  renderTodos() {
+    this.todoList.forEach(todo => todo.render());
+  }
+}
+
+class Todo {
+  constructor(task, state) {
+    this.task = task;
+    this.state = state || 'open';
+    this.template = `<p class="${this.state}">${this.task}</p>`
+  }
+
+  update(task, state) {
+    this.task = task;
+    this.state = state;
+  }
+}
+
+let myTodoList = new TodoList();
+myTodoList.addTodo(new Todo('Commit me', 'open'));
